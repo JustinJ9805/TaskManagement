aeb724b595d44d2c86cb2e5b9a25f475f54f163d
isaias <isaias.villa123456@gmail.com>
2023-05-29 22:59:30 -0400

    update on new employee & new project

--- taskmanagement/src/scenes/newProject/index.jsx
+++ taskmanagement/src/scenes/newProject/index.jsx
@@ -4,8 +4,7 @@ import { FormControl, TextField, Button, FormLabel } from "@mui/material";
 
 const NewProject = () => {
 
-
-    return(
+    return (
         <div>
             <FormControl>
                 <div>
@@ -14,14 +13,32 @@ const NewProject = () => {
                     </FormLabel>
                     <TextField color="warning" required={true} id="projectName" />
                 </div>
+                <div>
+                    <FormLabel htmlFor="name">
+                        <h3>Due date:</h3>
+                    </FormLabel>
+                    <TextField color="warning" required={true} id="name" />
+                </div>
+                <div>
+                    <FormLabel htmlFor="name">
+                        <h3>description:</h3>
+                    </FormLabel>
+                    <TextField color="warning" required={true} id="name" />
+                </div>
+                <div>
+                    <FormLabel htmlFor="name">
+                        <h3>User:</h3>
+                    </FormLabel>
+                    <TextField color="warning" required={true} id="name" />
+                </div>
 
                 <Button><h3>Create Project</h3></Button>
             </FormControl>
 
-            
-            
+
+
         </div>
-        
+
     )
 }
 
