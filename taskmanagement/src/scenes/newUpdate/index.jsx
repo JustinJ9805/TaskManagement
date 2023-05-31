ab717107f6d826c605a1fcfcf31cf650e7c9036d
isaias <isaias.villa123456@gmail.com>
2023-05-31 15:31:29 -0400

    tasks and updates

--- taskmanagement/src/scenes/newUpdate/index.jsx
+++ taskmanagement/src/scenes/newUpdate/index.jsx
@@ -1,8 +1,54 @@
 import React from "react";
+import { FormControl, TextField, Button, FormLabel } from "@mui/material";
+import axios from "axios";
 
 const NewUpdate = () => {
     return(
-        <h3>Hi</h3>
+            <div>
+          <h3>Create New update</h3>
+    
+          <FormControl>
+            <div>
+              <FormLabel htmlFor="taskName">
+                <h3>Update Name:</h3>
+              </FormLabel>
+              <TextField
+                color="warning"
+                required={true}
+                id="taskName"
+                //value={taskName}
+                //onChange={(e) => setTaskName(e.target.value)}
+              />
+            </div>
+    
+            <div>
+              <FormLabel htmlFor="description">
+                <h3>Note:</h3>
+              </FormLabel>
+              <TextField
+                color="warning"
+                required={true}
+                id="description"
+                //value={description}
+                //onChange={(e) => setDescrpition(e.target.value)}
+              />
+            </div>
+            <div>
+              <FormLabel htmlFor="dueDate">
+                <h3>Due Date:</h3>
+              </FormLabel>
+              <TextField
+                color="warning"
+                required={true}
+                id="dueDate"
+                //value={dueDate}
+                //onChange={(e) => setDueDate(e.target.value)}
+              />
+            </div>
+            
+            
+          </FormControl>
+        </div>
     )
 }
 
